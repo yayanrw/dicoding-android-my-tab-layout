bd527b441f248a18470e22dd9bdd5949986c1ecc
yayan-msglow <yayanrahmat@msglowid.com>
2021-10-31 22:33:53 +0700

    add 1 tab and using 1 fragment

--- app/src/main/java/com/example/mytablayout/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/mytablayout/SectionsPagerAdapter.kt
@@ -5,16 +5,12 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+
     override fun createFragment(position: Int): Fragment {
-        var fragment: Fragment? = null
-        when (position) {
-            0 -> fragment = HomeFragment()
-            1 -> fragment = ProfileFragment()
-        }
-        return fragment as Fragment
+        return HomeFragment.newInstance(position + 1)
     }
 
     override fun getItemCount(): Int {
-        return 2
+        return 3
     }
 }
